92b91c213b6ad5368701272be9d6c1c5b0522b93
zacharybaca <zachary.baca@msn.com>
2023-09-16 15:22:52 -0500

    changes

--- quicksort.js
+++ quicksort.js
@@ -1,19 +1,52 @@
 function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-  // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  let pivot = arr[0];
 
-  // Pick the first value as the pivot
+  let left = [];
+  let right = [];
 
-  // Orient the pivot so that...
-      // every number smaller than the pivot is to the left
-      // every number larger (or equal) than the pivot is to the right
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
 
-  // Recursively sort the left
-  // Recursively sort the right
+  }
+  let leftSort = quicksort(left);
+  let rightSort = quicksort(right);
+  return [...leftSort, pivot, ...rightSort]
+  // let leftArr = [];
+  // let rightArr = [];
+  // // Check if the input is length 1 or less
+  //   // If so, it's already sorted: return
+  //   console.log('Repeat: ', arr);
+  // if (arr.length <= 1) {
+  //   return arr;
+  // }
+  // // Pick the first value as the pivot
+  // let pivot = arr(Math.floor(arr.length / 2));
+  // // Orient the pivot so that...
+  //     // every number smaller than the pivot is to the left
+  //     // every number larger (or equal) than the pivot is to the right
+  // for (const element of arr) {
+  //   if (pivot > element) {
+  //     rightArr.push(element);
+  //   } else {
+  //     leftArr.push(element);
+  //   }
+  // }
 
-  // Return the left, pivot and right in sorted order
+  // // Recursively sort the left
+  // let LA = quicksort(leftArr);
+  // // Recursively sort the right
+  // let RA = quicksort(rightArr);
 
+  // // Return the left, pivot and right in sorted order
+  // return LA + pivot + RA;
 }
 
 
